6f3469cf49adace8aa61103c66d86a48ce609ef1
Clifford Mackay <cliff@cmartme.com>
2021-01-31 12:57:52 -0800

    * Changed Indexes to ValueTuple so we get the nice struct comparisons

--- BevNet/Indexes.cs
+++ BevNet/Indexes.cs
@@ -6,7 +6,7 @@ namespace cmArt.BevNet
 {
     public static class Indexes
     {
-        public static ValueTuple<string,string> SupplierCode_SupplierPartNum_Key(IPriceFile priceFile)
+        public static (string SupplierCode, string SupplierPart) SupplierCode_SupplierPartNum_Key(IPriceFile priceFile)
         {
             return new ValueTuple<string, string>(priceFile.WHOLESALER, priceFile.PROD_ITEM);
         }
