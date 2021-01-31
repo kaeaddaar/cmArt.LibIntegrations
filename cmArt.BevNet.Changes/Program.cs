f7d31ddf532eb8fdb05a56352b325850d79317e8
Clifford Mackay <cliff@cmartme.com>
2021-01-31 12:57:24 -0800

    * New project for console app to swap/include supplier names for ones from System Five and added appsettings.json support

---
+++ cmArt.BevNet.Changes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+
+namespace cmArt.BevNet.Changes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin");
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            Console.WriteLine($"Path: {config["SourceDirectory"]}");
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+
+    }
+}
