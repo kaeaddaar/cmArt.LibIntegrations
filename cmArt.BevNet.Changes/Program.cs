3d655c4b15c1f295a2a57869c22a84a426b93aeb
Clifford Mackay <cliff@cmartme.com>
2021-02-04 13:27:34 -0800

    * Added structure for new console app just for the simplified scenario of making changes to source file so that we can load updates

--- cmArt.BevNet.Changes/Program.cs
+++ cmArt.BevNet.Changes/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
-
+using cmArt.LibIntegrations.CsvFileReaderService;
+using cmArt.BevNet;
+using System.Linq;
 
 namespace cmArt.BevNet.Changes
 {
@@ -14,6 +16,21 @@ namespace cmArt.BevNet.Changes
             IConfiguration config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .Build();
+
+            IEnumerable<IPriceFile> BevNetRecords = new List<PriceFile>();
+            string PathAndFile = config["SourceDirectory"] + config["FileName"];
+            const bool DontSuppressRecordErrors = false;
+
+            BevNetRecords = FileReaderGeneric<PriceFile>.ReadFile
+            (   PathAndFile: PathAndFile
+                , SupressRecordErrors: DontSuppressRecordErrors
+            );
+
+            int MaxRecordsToDisplay = 10;
+            for (int i = 0; i < MaxRecordsToDisplay; i++)
+            {
+                Console.WriteLine(BevNetRecords.Skip(i).FirstOrDefault().PROD_ITEM);
+            }
 
             Console.WriteLine($"Path: {config["SourceDirectory"]}");
 
