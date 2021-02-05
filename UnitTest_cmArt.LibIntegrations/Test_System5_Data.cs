3c0ddec9305395fb0e00ecfd6cddb20425543d9e
Clifford Mackay <cliff@cmartme.com>
2021-02-04 23:50:28 -0800

    * Test System Five Data table AltSuply for Trailing Spaces and Nulls

--- UnitTest_cmArt.LibIntegrations/Test_System5_Data.cs
+++ UnitTest_cmArt.LibIntegrations/Test_System5_Data.cs
@@ -23,7 +23,7 @@ namespace UnitTest_cmArt.LibIntegrations
             // Passing in a non-clean record to the constructor of a clean record will clean the origin
             AltSuplyRecord.PartNumber = HelloWorld_TwoTrailingSpaces;
             AltSuply_Clean clean = new AltSuply_Clean(AltSuplyRecord);
-            Assert.AreEqual(AltSuplyRecord.PartNumber, HelloWorld_NoTrailingSpaces);
+            Assert.AreEqual(HelloWorld_NoTrailingSpaces, AltSuplyRecord.PartNumber);
             Assert.AreEqual(" ", AltSuplyRecord.PartNumber.Substring(0, 1));
 
             // Clean on the clean record will clean the non-clean record
@@ -49,7 +49,7 @@ namespace UnitTest_cmArt.LibIntegrations
 
             // -- Quick tests to check each other cleaned field
             AltSuply A = new AltSuply();
-            AltSuply_Clean B = new AltSuply_Clean(A);
+            AltSuply_Clean B = new AltSuply_Clean(A); // Will find uninitialized or null field issues
             A.PartNumber = HelloWorld_TwoTrailingSpaces;
             Assert.AreEqual(HelloWorld_TwoTrailingSpaces, A.PartNumber);
             Assert.AreEqual(HelloWorld_NoTrailingSpaces, B.PartNumber); 
@@ -59,6 +59,51 @@ namespace UnitTest_cmArt.LibIntegrations
             // --
 
 
+        }
+        [TestMethod]
+        public void Test_that_null_values_dont_break_clean_field_methods()
+        {
+            AltSuply A = new AltSuply();
+            A.PackSize = null;
+            A.PartNumber = null;
+            A.Preferred = null;
+
+            AltSuply_Clean B = new AltSuply_Clean(A);
+            
+            Assert.AreEqual(false, A.PackSize == null);
+            Assert.AreEqual(false, A.PartNumber == null);
+            Assert.AreEqual(false, A.Preferred == null);
+
+            A.PackSize = null;
+            A.PartNumber = null;
+            A.Preferred = null;
+
+            Assert.AreEqual(false, B.PackSize == null);
+            Assert.AreEqual(false, B.PartNumber == null);
+            Assert.AreEqual(false, B.Preferred == null);
+        }
+        [TestMethod]
+        public void Test_that_nullable_props_remove_null()
+        {
+            AltSuply A = new AltSuply();
+            AltSuply_Clean B = new AltSuply_Clean(A);
+            B.PackSize = null;
+            B.PartNumber = null;
+            B.Preferred = null;
+            AltSuply_Clean_Null_Checks(B);
+        }
+        [TestMethod]
+        public void Test_For_null_values_in_uninitialized_AltSuply() // putting any brains in the IAltSuply_clean
+        {
+            AltSuply A = new AltSuply();
+            AltSuply_Clean B = new AltSuply_Clean(A);
+            AltSuply_Clean_Null_Checks(B);
+        }
+        private void AltSuply_Clean_Null_Checks(AltSuply_Clean A)
+        {
+            Assert.IsFalse(A.PackSize == null);
+            Assert.IsFalse(A.PartNumber == null);
+            Assert.IsFalse(A.Preferred == null);
         }
 
     }
