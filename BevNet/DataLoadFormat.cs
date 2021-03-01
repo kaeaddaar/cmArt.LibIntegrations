259703e7e77ddf2652ae2ef6c5ed7282a4a0048c
Clifford Mackay <cliff@cmartme.com>
2021-03-01 10:09:18 -0800

    * Save point: Writting Data Load File with missing info

--- BevNet/DataLoadFormat.cs
+++ BevNet/DataLoadFormat.cs
@@ -1,17 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using cmArt.LibIntegrations;
+using FileHelpers;
+
 
 namespace cmArt.BevNet
 {
-    public class DataLoadFormat : ICommonFields, IDataLoadFormat
+    [DelimitedRecord(",")]
+    public class DataLoadFormat : ICommonFields, IDataLoadFormat, ICopyable<IDataLoadFormat>
     {
+        [FieldHidden]
         private ICommonFields _CommonFields;
+        public DataLoadFormat()
+        {
+            _CommonFields = new CommonFields();
+        }
+        public void init(ICommonFields CommonFields)
+        {
+            _CommonFields = CommonFields ?? new CommonFields();
+        }
+        public IDataLoadFormat CopyFrom(IDataLoadFormat IFrom)
+        {
+            this.Cat = IFrom.Cat;
+            this.InvUnique = IFrom.InvUnique;
+            this.PartNumber = IFrom.PartNumber;
+            this.PriceSchedule1_MSRP = IFrom.PriceSchedule1_MSRP;
+            this.PriceSchedule2_MinPrice = IFrom.PriceSchedule2_MinPrice;
+            this.SupplierCode = IFrom.SupplierCode;
+            this.SupplierName = IFrom.SupplierName;
+            this.SupplierPartNumber = IFrom.SupplierPartNumber;
+            this.WholesaleCost = IFrom.WholesaleCost;
+            return this;
+        }
+
         public int InvUnique { get; set; }
+        [FieldQuoted]
+        [FieldNullValue(typeof(string), "")]
         public string Cat { get; set; }
+        [FieldQuoted]
+        [FieldNullValue(typeof(string), "")]
         public string PartNumber { get; set; }
+        [FieldQuoted]
+        [FieldNullValue(typeof(string), "")]
         public string SupplierName { get => _CommonFields.SupplierName; set => _CommonFields.SupplierName = value; }
+        [FieldQuoted]
+        [FieldNullValue(typeof(string), "")]
         public string SupplierPartNumber { get => _CommonFields.SupplierPartNumber; set => _CommonFields.SupplierPartNumber = value; }
+        [FieldQuoted]
+        [FieldNullValue(typeof(string), "")]
         public string SupplierCode { get => _CommonFields.SupplierCode; set => _CommonFields.SupplierCode = value; }
         public decimal WholesaleCost { get => _CommonFields.WholesaleCost; set => _CommonFields.WholesaleCost = value; }
         public decimal PriceSchedule1_MSRP { get => _CommonFields.PriceSchedule1_MSRP; set => _CommonFields.PriceSchedule1_MSRP = value; }
--- cmArt.BevNet.Changes/Program.cs
+++ cmArt.BevNet.Changes/Program.cs
@@ -5,7 +5,11 @@ using cmArt.LibIntegrations.CsvFileReaderService;
 using cmArt.BevNet;
 using System.Linq;
 using cmArt.LibIntegrations.OdbcService;
+using cmArt.LibIntegrations.PagedJsonService;
 using cmArt.System5.Inventory;
+using cmArt.BevNet.System5;
+using cmArt.LibIntegrations.GenericJoinsService;
+using FileHelpers;
 
 namespace cmArt.BevNet.App
 {
@@ -37,21 +41,47 @@ namespace cmArt.BevNet.App
             Console.WriteLine($"Path: {config["SourceDirectory"]}");
 
             Console.WriteLine("Loading Inventory From Real Windward");
+            
             IEnumerable<IS5InvAssembled> InvAss = GetDataFromSystemFive(config);
             Console.WriteLine("Finished - Loading Inventory From Real Windward");
 
             // A: Get XRef from Account for AUnique,BankInfo pairs
-            GetXRefFromSupplierRecords();
+            IEnumerable<QryAccount> accts = GetXRefFromSupplierRecords(config);
 
             // B: Attach BankInfo / wholesaler to Assembled Inventory to make InvAss, wholesaler pairs
             //   GenericJoins service will do this
-            MakePairs();
+            IEnumerable<Tuple<IS5InvAssembled, QryAccount>> result = MakePairs(InvAss, accts);
 
             // C: create a facade to go from InvAss, wholesaler pairs to a data load format
+            IEnumerable<AdaptToDataLoadFormat> adapters = result.Select(p =>
+                {
+                    AdaptToDataLoadFormat tmp = new AdaptToDataLoadFormat();
+                    tmp.Init(new ValueTuple<IS5InvAssembled, QryAccount>(p.Item1, p.Item2));
+                    return tmp;
+                }
+            );
 
+            // At this point I believe I can focus on putting into CSV file. So implement ICopyable<T>
+            IEnumerable<DataLoadFormat> DataLoad = adapters.Select(a => 
+            {
+                DataLoadFormat tmp = new DataLoadFormat();
+                tmp.CopyFrom(a); // .CopyFrom is from ICopyable
+                return tmp;
+            });
+
+            var engine = new FileHelperAsyncEngine<DataLoadFormat>();
+            using (engine.BeginWriteFile(config["SourceDirectory"] + "S5InventoryDataLoad.csv"))
+            {
+                foreach(var record in DataLoad)
+                {
+                    engine.WriteNext(record);
+                }
+            }
+            
             // D: create an adapter to go from the data load format to Common fields - PriceFileAdapter
 
-            // E: get common fields form BevNets PriceFileAdapter
+            // E: get common fields from BevNets PriceFileAdapter
+
 
             // F: apply rehydrator to step Ds adapter and step Es common fields
 
@@ -60,14 +90,52 @@ namespace cmArt.BevNet.App
             Console.WriteLine("Done");
             Console.ReadKey();
         }
-        private static void MakePairs()
+        private static IEnumerable<Tuple<IS5InvAssembled, QryAccount>> MakePairs(IEnumerable<IS5InvAssembled> InvAss, IEnumerable<QryAccount> accts)
         {
+            Func<QryAccount, int> QryAccount_Index = (record) =>
+            {
+                return record.AUnique;
+            };
 
+            IEnumerable<Tuple<IS5InvAssembled, QryAccount>> result = GenericJoins<IS5InvAssembled, QryAccount, int>
+                .InnerJoin(InvAss, accts, IS5InvAssembled_Indexes.SupplierUnique_Key, QryAccount_Index);
+            return result;
         }
-        private static void GetXRefFromSupplierRecords()
+        private static IEnumerable<QryAccount> GetXRefFromSupplierRecords(IConfiguration config)
         {
             // A: Get XRef from Account for AUnique,BankInfo pairs
+            QryAccountContext context = new QryAccountContext();
+            Options opt = OdbcOptions.GetOptions(config["System5DSN"]);
+            context.init(opt);
 
+            Func<QryAccount, QryAccount> clean = (acct) =>
+            {
+                acct.AName = acct.AName.TrimEnd();
+                acct.BankInfo = acct.BankInfo.TrimEnd();
+                return acct;
+            };
+
+            IEnumerable<QryAccount> records = context._Records.Select(r => clean(r));
+            return records;
+        }
+        private static IEnumerable<IS5InvAssembled> GetDataFromJson(IConfiguration config)
+        {
+            string CachedFilesDirectory = config["SourceDirectory"];
+
+            Options opt = PagedJsonOptions_S5Inventory.GetOptions(CachedFilesDirectory, new List<string>());
+            OdbcContext_S5Inventory context = new OdbcContext_S5Inventory(opt);
+
+            // Assemble Inventory
+            S5Inventory InvRaw = new S5Inventory
+            (
+                AltSuply_Records: context.AltSuplies
+                , Comments_Records: context.CommentsLines
+                , Inventry_27_Records: context.Inventry_27s
+                , InvPrice_Records: context.InvPrices
+                , Stok_Records: context.StokLines
+            );
+            IEnumerable<IS5InvAssembled> InvAss = InvRaw.ToAssembled();
+            return InvAss;
         }
         private static IEnumerable<IS5InvAssembled> GetDataFromSystemFive(IConfiguration config)
         {
