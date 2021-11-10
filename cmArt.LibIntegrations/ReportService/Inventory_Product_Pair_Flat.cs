f244772034e679f59af51ccf0de6381ca6ffca59
Clifford Mackay <cliff@cmartme.com>
2021-11-10 11:10:40 -0800

    Comment out the code that's not compiling so we can merge changes and continue later.

--- cmArt.LibIntegrations/ReportService/Inventory_Product_Pair_Flat.cs
+++ cmArt.LibIntegrations/ReportService/Inventory_Product_Pair_Flat.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using FileHelpers;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using FileHelpers;
 
 
-namespace cmArt.LibIntegrations.ReportService
-{
-    [DelimitedRecord(",")]
-    public class Inventory_Product_Pair_Flat : IInventory_Pair_Flat
-    {
-        public int LeftInvUnique { get; set; }
+//namespace cmArt.LibIntegrations.ReportService
+//{
+//    [DelimitedRecord(",")]
+//    public class Inventory_Product_Pair_Flat : IInventory_Pair_Flat
+//    {
+//        public int LeftInvUnique { get; set; }
 
-        [FieldQuoted]
-        [FieldNullValue(typeof(string), "")]
-        public string LeftCat { get; set; }
+//        [FieldQuoted]
+//        [FieldNullValue(typeof(string), "")]
+//        public string LeftCat { get; set; }
 
-        [FieldQuoted]
-        [FieldNullValue(typeof(string), "")]
-        public string LeftPartNumber { get; set; }
+//        [FieldQuoted]
+//        [FieldNullValue(typeof(string), "")]
+//        public string LeftPartNumber { get; set; }
 
-        [FieldQuoted]
-        [FieldNullValue(typeof(string), "")]
-        public string LeftDescription { get; set; }
+//        [FieldQuoted]
+//        [FieldNullValue(typeof(string), "")]
+//        public string LeftDescription { get; set; }
 
-        public int RightInvUnique { get; set; }
+//        public int RightInvUnique { get; set; }
 
-        [FieldQuoted]
-        [FieldNullValue(typeof(string), "")]
-        public string RightCat { get; set; }
+//        [FieldQuoted]
+//        [FieldNullValue(typeof(string), "")]
+//        public string RightCat { get; set; }
 
-        [FieldQuoted]
-        [FieldNullValue(typeof(string), "")]
-        public string RightPartNumber { get; set; }
+//        [FieldQuoted]
+//        [FieldNullValue(typeof(string), "")]
+//        public string RightPartNumber { get; set; }
 
-        [FieldQuoted]
-        [FieldNullValue(typeof(string), "")]
-        public string RightDescription { get; set; }
-    }
+//        [FieldQuoted]
+//        [FieldNullValue(typeof(string), "")]
+//        public string RightDescription { get; set; }
+//    }
 
-}
+//}
