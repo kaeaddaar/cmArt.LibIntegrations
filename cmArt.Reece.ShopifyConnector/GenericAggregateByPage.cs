ebcc0a6e0bf9709ce84e4043ddb089658e8f94e4
Clifford Mackay <cliff@cmartme.com>
2022-01-23 04:51:45 -0800

    * Add ToPages to help with aggregates

---
+++ cmArt.Reece.ShopifyConnector/GenericAggregateByPage.cs
@@ -0,0 +1,19 @@
+﻿using cmArt.System5.Inventory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cmArt.Reece.ShopifyConnector
+{
+    public static class GenericAggregateByPage<TGroupMe>
+    {
+        public static List<List<TGroupMe>> ToPages(IEnumerable<TGroupMe> Objs, int PageSize)
+        {
+            int count = 0;
+            Func<TGroupMe, int> fGetPageNumber = (x) => (int)((count++ / PageSize) + 1);
+            List<List<TGroupMe>> Pages = GenericAggregateToLists<TGroupMe,int>.ToLists(Objs, fGetPageNumber);
+
+            return Pages;
+        }
+    }
+}
