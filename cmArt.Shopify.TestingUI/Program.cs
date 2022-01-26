bb72226dc3170322deb522831d9176a10efc92d0
Clifford Mackay <cliff@cmartme.com>
2022-01-26 07:15:06 -0800

    * Added a testing UI project and loading JSON files that I cached form S5 and From Shopify.  - Still need to do QTY  - Create a filter and compare for checking on single records

---
+++ cmArt.Shopify.TestingUI/Program.cs
@@ -0,0 +1,11 @@
+using cmArt.Shopify.TestingUI;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
