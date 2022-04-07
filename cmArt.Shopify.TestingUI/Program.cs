85bb312b443d5cbe46c37073221f6b8b932d65a9
Clifford Mackay <cliff@cmartme.com>
2022-04-06 20:43:19 -0700

    * Rolled back to .net 3.1  - There were several changes to libraries etc. and replacing things that only had .net6 support  - Had to clear out some async await functionality.

--- cmArt.Shopify.TestingUI/Program.cs
+++ cmArt.Shopify.TestingUI/Program.cs
@@ -2,11 +2,13 @@ using cmArt.Shopify.TestingUI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Cors;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+//builder.RootComponents.Add<HeadOutlet>("head::after"); // Re-Add with .Net6.0
 
 //builder.Logging.SetMinimumLevel(LogLevel.Debug);
 //builder.Logging.AddProvider(new CustomLoggingProvider());
@@ -17,4 +19,5 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 //    options.AddDefaultPolicy(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 //});
 
-await builder.Build().RunAsync();
+//await builder.Build().RunAsync(); // re-add with .Net6.0
+builder.Build().RunAsync();
