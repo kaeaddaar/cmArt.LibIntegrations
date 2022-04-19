9979796f9ed4a7e698cf1f6914c581a25b45c24e
Clifford Mackay <cliff@cmartme.com>
2022-04-19 12:55:12 -0700

    * Added FileNameService so that I don't have to re-do any file name and path parsing

---
+++ UnitTest_cmArt.LibIntegrations/Test_FileNameService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using cmArt.LibIntegrations.FileNamesService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTest_cmArt.LibIntegrations
+{
+    [TestClass]
+    public class Test_FileNameService
+    {
+        [TestMethod]
+        public void GetExtension()
+        {
+            string FilePathAndName = "C:\\temp\\fileone.txt";
+            FileNameService fpn = new FileNameService(FilePathAndName);
+            string extension = fpn.GetExtention();
+            Assert.AreEqual("txt", extension);
+        }
+        [TestMethod]
+        public void GetFileName()
+        {
+            string FilePathAndName = "C:\\temp\\fileone.txt";
+            FileNameService fpn = new FileNameService(FilePathAndName);
+            string fileName = fpn.GetFileName();
+            Assert.AreEqual("fileone.txt", fileName);
+        }
+        [TestMethod]
+        public void GetFolder()
+        {
+            string FilePathAndName = "C:\\temp\\fileone.txt";
+            FileNameService fpn = new FileNameService(FilePathAndName);
+            string filePath = fpn.GetPath();
+            Assert.AreEqual("C:\\temp\\", filePath);
+        }
+
+    }
+}
---
+++ cmArt.LibIntegrations/FileNamesService/FileNameService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace cmArt.LibIntegrations.FileNamesService
+{
+    public class FileNameService
+    {
+        private string _filePathAndName;
+        public FileNameService()
+        {
+            _filePathAndName = string.Empty;
+        }
+        public FileNameService(string FilePathAndName)
+        {
+            _filePathAndName = FilePathAndName ?? string.Empty;
+        }
+        public void Init(string FilePathAndName)
+        {
+            _filePathAndName = FilePathAndName ?? string.Empty;
+        }
+        public string GetFileName()
+        {
+            int IndexLastSlash = GetPosLastOccurance('\\');
+            int LenFilePathAndName = _filePathAndName.Length;
+            int LenFileName = LenFilePathAndName - IndexLastSlash;
+            string fileName = _filePathAndName.Substring(IndexLastSlash, LenFileName);
+            return fileName;
+        }
+        public string GetPath()
+        {
+            int LenFileName = _filePathAndName.Length;
+            int IndexLastSlash = GetPosLastOccurance('\\');
+            int LenExtension = LenFileName - IndexLastSlash;
+            string Path = _filePathAndName.Substring(0, IndexLastSlash);
+            return Path;
+        }
+        public string GetExtention()
+        {
+            if (_filePathAndName == string.Empty)
+            { return string.Empty; }
+
+            string backwards = string.Join(string.Empty, _filePathAndName.Reverse());
+            bool HasAnExtension = backwards.Contains('.');
+            
+            if (!HasAnExtension)
+            { return string.Empty; }
+
+            int LenFileName = _filePathAndName.Length;
+            int IndexLastDot = GetPosLastOccurance('.');
+            int LenExtension = LenFileName - IndexLastDot;
+            string Extension = _filePathAndName.Substring(IndexLastDot, LenExtension);
+            return Extension;
+        }
+        private int GetPosLastOccurance(char ToCheckFor)
+        {
+            int NotFound = 0;
+            if (_filePathAndName == string.Empty)
+            { return 0; }
+
+            string backwards = string.Join(string.Empty, _filePathAndName.Reverse());
+            bool CharacterExists = backwards.Contains(ToCheckFor);
+
+            if (!CharacterExists)
+            { return NotFound; }
+
+            int PosBackwards = backwards.IndexOf(ToCheckFor);
+            int LenFileName = _filePathAndName.Length;
+            int PosChar = LenFileName - PosBackwards;
+
+            return PosChar;
+        }
+    }
+}
