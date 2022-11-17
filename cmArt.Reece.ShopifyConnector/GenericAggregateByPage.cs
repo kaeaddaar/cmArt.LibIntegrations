117edd404e5c184f42b867082f12032b6ef6093a
Clifford Mackay <cliff@cmartme.com>
2022-11-17 13:25:53 -0800

    * Added check in GenericAggregateByPage to ensure number of records doesn't change * Added check in GenericAggregateToLists to ensure number of records doesn't change

--- cmArt.Reece.ShopifyConnector/GenericAggregateByPage.cs
+++ cmArt.Reece.ShopifyConnector/GenericAggregateByPage.cs
@@ -1,6 +1,7 @@
 ﻿using cmArt.System5.Inventory;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace cmArt.Reece.ShopifyConnector
@@ -12,6 +13,16 @@ namespace cmArt.Reece.ShopifyConnector
             int count = 0;
             Func<TGroupMe, int> fGetPageNumber = (x) => (int)((count++ / PageSize) + 1);
             List<List<TGroupMe>> Pages = GenericAggregateToLists<TGroupMe,int>.ToLists(Objs, fGetPageNumber);
+
+            // checks
+            int orig_num_records = Objs.Count();
+            int number_of_records_within_pages = 0;
+            foreach(var page in Pages) { number_of_records_within_pages += page.Count(); }
+
+            if (orig_num_records != number_of_records_within_pages)
+            {
+                throw new Exception($"Wrong number of records in pages. Expected total number of records in pages to be {orig_num_records}, but there were {number_of_records_within_pages} across {Pages.Count()} pages");
+            }
 
             return Pages;
         }
--- cmArt.System5.Inventory/GenericAggregateToLists.cs
+++ cmArt.System5.Inventory/GenericAggregateToLists.cs
@@ -10,6 +10,16 @@ namespace cmArt.System5.Inventory
         public static List<List<TGroupMe>> ToLists(IEnumerable<TGroupMe> Objs, Func<TGroupMe, TGroupByIndex> fIndex)
         {
             var results = Objs.GroupBy(fIndex).Select(group => group.ToList()).ToList();
+
+            int orig_num_records = Objs.Count();
+            int number_of_records_within_pages = 0;
+            foreach (var page in results) { number_of_records_within_pages += page.Count(); }
+
+            if (orig_num_records != number_of_records_within_pages)
+            {
+                throw new Exception($"Wrong number of records in pages. Expected total number of records in pages to be {orig_num_records}, but there were {number_of_records_within_pages} across {results.Count()} pages");
+            }
+
             return results;
         }
     }
